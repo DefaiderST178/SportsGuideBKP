4ee3d98b062f80efef1b89d352c50e4b1c5581ae
Dmitry Belogorsky <dbelogorskiy@gmail.com>
2025-05-09 12:42:34 +0300

    Update 09/05/25 Release v1.0.3

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -21,5 +21,20 @@ namespace SportsGuideBKP
         {
             await Navigation.PushAsync(new OlympicWinnersView());
         }
+
+        private async void SummerNavBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SummerGamesView());
+        }
+
+        private async void WinterNavBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new WinterGamesView());
+        }
+
+        private async void WinnerNavBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new OlympicWinnersView());
+        }
     }
 }
